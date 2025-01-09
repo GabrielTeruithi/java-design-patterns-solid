61879dc4edcd758c69fe50e5b632ccc23bd9aaf0
Gabriel <gabrielt.oguma@gmail.com>
2025-01-09 11:13:23 -0300

    Implemented Open Close principle

--- src-code/src/openClose/ISPSubscriber.java
+++ src-code/src/openClose/ISPSubscriber.java
@@ -2,15 +2,8 @@ package openClose;
 
 import java.util.List;
 
-public class ISPSubscriber {
-
-    private Long subscriberId;
-
-    private String address;
-
-    private Long phoneNumber;
-
-    private int baseRate;
+// Open for Modification
+public class ISPSubscriber extends Subscriber {
 
     private long freeUsage;
 
@@ -18,67 +11,12 @@ public class ISPSubscriber {
 
     }
 
+    @Override
     public double calculateBill() {
         List<InternetSessionHistory.InternetSession> sessions = InternetSessionHistory.getCurrentSessions(subscriberId);
         long totalData = sessions.stream().mapToLong(InternetSessionHistory.InternetSession::getDataUsed).sum();
         long chargeableData = totalData - freeUsage;
-        return chargeableData*baseRate/100;
-    }
-
-    /**
-     * @return the subscriberId
-     */
-    public Long getSubscriberId() {
-        return subscriberId;
-    }
-
-    /**
-     * @param subscriberId the subscriberId to set
-     */
-    public void setSubscriberId(Long subscriberId) {
-        this.subscriberId = subscriberId;
-    }
-
-    /**
-     * @return the address
-     */
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     * @param address the address to set
-     */
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    /**
-     * @return the phoneNumber
-     */
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * @param phoneNumber the phoneNumber to set
-     */
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * @return the baseRate
-     */
-    public int getBaseRate() {
-        return baseRate;
-    }
-
-    /**
-     * @param baseRate the baseRate to set
-     */
-    public void setBaseRate(int baseRate) {
-        this.baseRate = baseRate;
+        return chargeableData * baseRate / 100;
     }
 
     /**
@@ -95,5 +33,4 @@ public class ISPSubscriber {
         this.freeUsage = freeUsage;
     }
 
-    
 }
---
+++ src-code/src/openClose/VoipSubscriber.java
@@ -0,0 +1,13 @@
+package openClose;
+
+// Open for Modification
+public class VoipSubscriber extends Subscriber {
+
+    
+    @Override
+    public double calculateBill() {
+        System.out.println("Created New Voip Subscriber");
+        return 0.0;
+    }
+    
+}
