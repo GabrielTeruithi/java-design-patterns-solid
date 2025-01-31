ab337ae98bdc4a9ed368cd9ba4dea3dca61fb67b
Gabriel Teruithi <gabrielt.oguma@gmail.com>
2025-01-31 11:56:08 -0300

    Proxy implemented

---
+++ src-code/src/designpatterns/structural/proxy/dynamic/Client.java
@@ -0,0 +1,19 @@
+package designpatterns.structural.proxy.dynamic;
+
+
+
+import designpatterns.creational.objectpool.Point2D;
+import designpatterns.structural.proxy.Image;
+import designpatterns.structural.proxy.ImageFactory;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Image img = ImageFactory.getImage("A.bmp");
+		img.setLocation(new Point2D(-10, 0));
+		System.out.println(img.getLocation());
+		System.out.println("*****************************");
+		img.render();
+		
+	}
+}
