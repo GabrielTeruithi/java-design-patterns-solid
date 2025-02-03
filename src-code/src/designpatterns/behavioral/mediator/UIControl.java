06c1d4e7eff287419a00d70b335f583e9b31913b
Gabriel Teruithi <gabrielt.oguma@gmail.com>
2025-02-03 15:03:22 -0300

    implemented mediator

---
+++ src-code/src/designpatterns/behavioral/mediator/UIControl.java
@@ -0,0 +1,11 @@
+package designpatterns.behavioral.mediator;
+
+//Abstract colleague
+public interface UIControl {
+	
+	void controlChanged(UIControl control);
+	
+	String getControlValue();
+	
+	String getControlName();
+}
